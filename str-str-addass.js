4d2ac6192a3e9cd6e3a4b5f7948aee8c7b640d53
João Viana <joaopadraoviana@gmail.com>
2018-08-10 10:03:56 +0200

    Update str-str-addass.js

--- str-str-addass.js
+++ str-str-addass.js
@@ -1,12 +1,12 @@
+//str_str
 debugger;
 
 let str_1 = "1";
 let str_str;
 { // str_1 += "1";
-  str_1 = str_1 + '1';
-  str_str = str_1;
+  str_1 = str_1 + "1";
+  str_str = str_1; 
 };
-
-let actual_str_str = "1";
-actual_str_str += "1";
-console.assert(str_str === actual_str_str, "fail: str_str");
+let actual = "1";
+actual += "1";
+console.assert(str_str === actual, "fail: str_str");
