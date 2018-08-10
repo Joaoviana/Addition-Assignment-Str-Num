bf77e40f8788f705fd1cd8c61977eb51a9ba8352
João Viana <joaopadraoviana@gmail.com>
2018-08-10 09:53:56 +0200

    Update num-num-addass.js

--- num-num-addass.js
+++ num-num-addass.js
@@ -7,5 +7,5 @@ let num_num;
   num_num = num_1;
 };
 let actual_str_num = 1;
-actual += 1;
-console.assert(num_num === actual, "fail: num_num");
+actual_str_num += 1;
+console.assert(num_num === actual_str_num, "fail: num_num");
