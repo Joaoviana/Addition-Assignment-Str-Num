08ff1905e0d3ba798be39f9582be81f74539f65d
João Viana <joaopadraoviana@gmail.com>
2018-08-10 10:04:35 +0200

    Update num-str-addass.js

--- num-str-addass.js
+++ num-str-addass.js
@@ -1,13 +1,13 @@
+
+// num_str
 debugger;
 
-let num_1 = 1;
+num_1 = 1;
 let num_str;
-
 { // num_1 += "1";
   num_1 = num_1 + "1";
   num_str = num_1;
 };
-
-let actual_num_str = 1;
-actual_num_str += "1";
-console.assert(num_str === actual_num_str, "fail: num_str");
+actual = 1;
+actual += "1";
+console.assert(num_str === actual, "fail: num_str");
